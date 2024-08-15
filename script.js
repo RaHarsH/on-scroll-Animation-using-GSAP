7311fa260d4f9a4214466ad872289a7099140cb3
Harshith R <harshith.raju0710@gmail.com>
2024-08-15 11:06:40 +0530

    added video frames to animate

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
+const canvas = document.querySelector('canvas');
+const context = canvas.getContext('2d');
+
 let frames = {
     currentIndex: 0,
-    maxIndex: 516,
+    maxIndex: 538,
 };
 
 let imagesLoaded = 0;
@@ -8,7 +11,7 @@ let images = [];
 
 const preloadImages = () => {
     for(let i = 1; i <= frames.maxIndex; i++) {
-        const imageUrl = `frames/compressed_image_${i.toString().padStart(4, '0')}.png`;
+        const imageUrl = `frames/frame_${i.toString().padStart(4, '0')}.jpg`;
         let img = new Image();
         img.src = imageUrl;
 
@@ -16,7 +19,7 @@ const preloadImages = () => {
             imagesLoaded++;
 
             if(imagesLoaded === frames.maxIndex) {
-                LoadImage(frames.currentIndex);
+                loadImage(frames.currentIndex);
                 startAnimation();
             }
         }
@@ -29,6 +32,47 @@ const loadImage = (index) => {
     if(index >= 0 && index <= frames.maxIndex) {
         const img = images[index];
 
-        // the rest of the code goes here
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight;
+
+        const scaleX = canvas.width / img.width;
+        const scaleY = canvas.height / img.height;
+
+        const scale = Math.max(scaleX, scaleY);
+
+        const newWidth = scale * img.width;
+        const newHeight = scale * img.height;
+
+        const offsetX = (canvas.width - newWidth) / 2;
+        const offsetY = (canvas.height - newHeight) / 2;
+
+        context.clearRect(0, 0, canvas.width, canvas.height);
+        context.imageSmoothingEnabled = true;
+        context.imageSmoothingQuality = "high";
+        context.drawImage(img, offsetX, offsetY, newWidth, newHeight);
+
+        frames.currentIndex = index;
     }
 }
+
+
+const startAnimation = () => {
+    let t1 = gsap.timeline({
+        scrollTrigger: {
+            trigger: '.parent',
+            start: 'top top',
+            end: 'bottom bottom',
+            scrub: 4,
+            markers: false,
+        }
+    });
+
+    t1.to(frames, {
+        currentIndex: frames.maxIndex,
+        onUpdate: () => {
+            loadImage(Math.floor(frames.currentIndex));
+        }
+    })
+}
+
+preloadImages();
